08a56e6eefaf47f8f71e9c80a9e724e01bcc99de
Lucas Lawes <k1736700@kingston.ac.uk>
2019-10-31 18:04:35 +0000

    Add some variable

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
+const someVariable = process.env.SOME_VARIABLE;
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello from test.recap.cloud v2!</h1>');
+    res.send('<h1>Hello from test.recap.cloud v2!</h1><p>Some variable is "${someVariable}"</p>');
 });
 
 app.listen(port, () => {
