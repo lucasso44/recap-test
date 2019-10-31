a8e43e2e3355a02e2e5daf7250fb49f6aa4d055b
Lucas Lawes <k1736700@kingston.ac.uk>
2019-10-31 18:07:49 +0000

    Fix some variable

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const someVariable = process.env.SOME_VARIABLE;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello from test.recap.cloud v2!</h1><p>Some variable is "${someVariable}"</p>');
+    res.send(`<h1>Hello from test.recap.cloud v2!</h1><p>Some variable is "${someVariable}"</p>`);
 });
 
 app.listen(port, () => {
